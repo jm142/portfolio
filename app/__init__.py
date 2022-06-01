75c928cf1221df61b48a4e863f7d8ebd9e77b105
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-06-01 14:54:12 -0400

    Prototype behaviour for populating template

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+jobs = [
+    {
+        'title': "MLH Fellow",
+        'description': "I signed an NDA I can't tell you"
+    },
+    {
+        'title': "Student",
+        'description': "working on a degree"
+    }
+]
+
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', jobs=jobs)
