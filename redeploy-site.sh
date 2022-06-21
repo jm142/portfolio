d73220f518052dd250102a484157381b9b8e69bc
jm142 <90632415+jm142@users.noreply.github.com>
2022-06-21 16:06:31 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,4 +3,6 @@ tmux kill-server
 cd portfolio
 git fetch && git reset origin/main --hard
 dnf install tmux
-tmux -d new -s $1 "python3.8 -m venv python3.8-virtualenv; source python3.8-virtualenv/bin/activate; pip3 install -r requirements.txt; flask run --host=0.0.0.0"
+source python3.8-virtualenv/bin/activate
+pip3 install -r requirements.txt;
+tmux new -ds $1 "flask run --host=0.0.0.0"
