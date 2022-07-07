e2ae97ac6295384eb10d67095baae77cbf3f823a
jm142 <90632415+jm142@users.noreply.github.com>
2022-07-07 15:18:03 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
                      password=os.getenv("MYSQL_PASSWORD"),
                      host=os.getenv("MYSQL_HOST"),
                      port=3306
+)
 
 class TimelinePost(Model):
     name = CharField()
