09bdbe4f1bd59656c85b8cad7a2b6c7dfe54eb90
jm142 <90632415+jm142@users.noreply.github.com>
2022-07-07 15:45:52 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,13 +7,13 @@ tmux kill-server
 git fetch && git reset origin/main --hard
 
 # Enter python venv and install dependencies
-source python3-virtualenv/bin/activate
+source python3.8-virtualenv/bin/activate
 
 # Install requirements
 pip install -r requirements.txt
 
 # Start Flask in a detached tmux session
 tmux new-session -d -s Flask
-tmux send-keys 'source python3-virtualenv/bin/activate' C-m
+tmux send-keys 'source python3.8-virtualenv/bin/activate' C-m
 tmux send-keys 'python3.8 -m flask run --host=0.0.0.0 --port=80' C-m
 tmux detach -s Flask
