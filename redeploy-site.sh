709959f83583948dcfbf0f3b9f4c2de11f9afdb6
jm142 <90632415+jm142@users.noreply.github.com>
2022-07-07 13:21:00 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,5 +5,5 @@ source python3-virtualenv/bin/activate
 python3.8 -m pip install -r requirements.txt
 tmux new-session -d -s Flask
 tmux send-keys 'source python3-virtualenv/bin/activate' C-m
-tmux send-keys 'python3.8 -m flask run --host=0.0.0.0 --port=80' C-m
+tmux send-keys 'python3.8 -m flask run --host=0.0.0.0' C-m
 tmux detach -s Flask
