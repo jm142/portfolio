af1f3b222cf2c6dc463a50473e085dcef6fe9975
jm142 <90632415+jm142@users.noreply.github.com>
2022-07-07 15:26:36 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,45 +1,53 @@
-from playhouse.shortcuts import model_to_dict
-import datetime
-from peewee import *
 import json
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_nav import Nav, register_renderer
 from flask_nav.elements import Navbar, View
+from peewee import MySQLDatabase, Model, DateTimeField, TextField, CharField
+import datetime
+
+from playhouse.shortcuts import model_to_dict
 
 from generate_map import generate_map
 from navbar_renderer import NavbarRenderer
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-                     user=os.getenv("MYSQL_USER"),
-                     password=os.getenv("MYSQL_PASSWORD"),
-                     host=os.getenv("MYSQL_HOST"),
-                     port=3306
+load_dotenv()
+app = Flask(__name__)
+
+# Initiate MySQL database
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
 )
+
 
 class TimelinePost(Model):
     name = CharField()
     email = CharField()
     content = TextField()
-    created_at = DateTimeField(default=datetime.datetime.now)
+    created_at = DateTimeField(default=datetime.datetime.now())
 
     class Meta:
         database = mydb
 
+
+# Connect to the DB and create table for the app
 mydb.connect()
 mydb.create_tables([TimelinePost])
-load_dotenv()
-app = Flask(__name__)
 
-print(mydb)
 # Create dynamic navbar instance
 nav_bar = Navbar('Navigation',
                  View('Home', 'index'),
                  View('Work Experience', 'experience'),
-                 View('Hobbies', 'hobbies')
+                 View('Hobbies', 'hobbies'),
+                 View('Timeline', 'timeline')
                  )
+
 # Initialize and register Nav library
 nav = Nav()
 nav.register_element('navigation', nav_bar)
@@ -75,13 +83,21 @@ def hobbies():
     return render_template('hobbies.html', json_data=json_data, title="Hobbies", url=os.getenv("URL"))
 
 
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline", posts=get_posts()['timeline_posts'])
+
+
 # This route is only used in an iframe, so it doesn't need to be on the navbar
 @app.route('/map')
 def travel_map():
     return render_template('generated/generated_map.html')
 
+
+# Create post endpoint
 @app.route('/api/timeline_post', methods=['POST'])
-def post_time_line_post():
+def post_post():
+    print(request.values.to_dict())
     name = request.form['name']
     email = request.form['email']
     content = request.form['content']
@@ -89,14 +105,12 @@ def post_time_line_post():
 
     return model_to_dict(timeline_post)
 
+
 @app.route('/api/timeline_post', methods=['GET'])
-def get_time_line_post():
+def get_posts():
     return {
         'timeline_posts': [
             model_to_dict(p)
             for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
-            ]
-        }
-@app.route('/timeline')
-def timeline():
-    return render_template('timeline.html', title="Timeline")
+        ]
+    }
