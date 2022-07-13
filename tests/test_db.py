58adec8aa896f7ef451cb2a52cb317e657e817c5
jm142 <90632415+jm142@users.noreply.github.com>
2022-07-13 10:33:36 -0400

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,5 @@
+# test_db.py
+
 import unittest
 from peewee import *
 
@@ -5,31 +7,37 @@ from app import TimelinePost
 
 MODELS = [TimelinePost]
 
-# Create in-memory sqlite to run tests
+# use an in-memoru SQLite for tests.
 test_db = SqliteDatabase(':memory:')
 
-
-# TimelinePost unit test class
 class TestTimelinePost(unittest.TestCase):
-    def setUp(self):
-        # Bind models to test db
-        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+	def setUp(self):
+		# Bind model classes to test db. Since we have a complete list of
+		# all models, we do not need to recursively bind dependencies
+		test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
 
-        test_db.connect()
-        test_db.create_tables(MODELS)
+		test_db.connect()
+		test_db.create_tables(MODELS)
 
-    def tearDown(self):
-        # Destroy the db
-        test_db.drop_tables(MODELS)
-        test_db.close()
+	def tearDown(self):
+		# Not strictly necessary since SQLite in-memory databases only live
+		# for the duration of the connection, and in the same next step we close
+		# the connection...but a good practice all the same.
+		test_db.drop_tables(MODELS)
 
-    def test_timeline_post(self):
-        # Create 2 timeline posts
-        test_post_1 = TimelinePost.create(name="Test Poster 1", email="Tester1@example.com", content="Hello! Test #1!")
-        # Make sure first created post has an ID of 1
-        assert test_post_1.id == 1
+		# Close connection to db.
+		test_db.close()
 
-        # Do same for a second post
-        test_post_2 = TimelinePost.create(name="Test Poster 2", email="Tester2@example.com", content="Hello! Test #2!")
-        assert test_post_2.id == 2
-
+	def test_timeline_post(self):
+		# Create 2 timeline posts.
+		first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
+		assert first_post.id == 1
+		assert first_post.name == 'John Doe'
+		assert first_post.email == 'john@example.com'
+		assert first_post.content == 'Hello world, I\'m John!'
+		second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
+		assert second_post.id == 2
+		assert second_post.name == 'Jane Doe'
+		assert second_post.email == 'jane@example.com'
+		assert second_post.content == 'Hello world, I\'m Jane!'
+		# TODO: Get timeline posts and assert that they are correct.
